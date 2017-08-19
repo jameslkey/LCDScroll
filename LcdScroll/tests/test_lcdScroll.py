86558cfebc248bd9aa41dc22710b975e3a086068
James L. Key <james@bluepenguinsolutions.com>
2017-08-19 16:39:03 -0400

    Added lots of tests and have begun to make a sub-class of AdafruitCharLCD

--- LcdScroll/tests/test_lcdScroll.py
+++ LcdScroll/tests/test_lcdScroll.py
@@ -14,6 +14,8 @@ from unittest import TestCase
 from LcdScroll import LcdScroll
 from unittest import skip
 from LcdScroll import LcdScrollEx
+import pytest
+import time
 
 import os
 if os.name == 'nt':
@@ -24,7 +26,11 @@ else:
 
 class TestLcdScroll(TestCase):
     def setUp(self):
-        self.display = Adafruit_CharLCDPlate(cols=16, lines=2)
+        self.columns = 20
+        self.lines = 4
+        self.display = Adafruit_CharLCDPlate(cols=self.columns, lines=self.lines)
+        self.display.clear()
+        self.display.set_backlight(0)
         self.lcd = LcdScroll(self.display)
 
     def test_message_set(self):
@@ -52,53 +58,88 @@ class TestLcdScroll(TestCase):
         self.fail()
 
     def test_display_size_set(self):
-        """
-        Simple test of size set method
+        r"""
+        Simple test of display_size set method
 
         """
         self.lcd.columns = 20
         self.lcd.lines = 4
-        self.assertIsInstance(self.lcd.display_size, tuple, 'Did not return tuple')
+        self.assertIsInstance(self.lcd.display_size, tuple, 'display_size() Did not return tuple')
         self.assertEqual(self.lcd.display_size, (20, 4), 'Unable to set display size')
         self.lcd.columns = 16
         self.lcd.lines = 2
         self.assertEqual(self.lcd.display_size, (16, 2), 'Unable to change display size')
 
-        def setcol():
+        def set_col():
             self.lcd.columns = 0
 
-        def setline():
+        def set_line():
             self.lcd.lines = -3
 
-        self.assertRaises(LcdScrollEx, setcol)
-        self.assertRaises(LcdScrollEx, setline)
+        self.assertRaises(LcdScrollEx, set_col)
+        self.assertRaises(LcdScrollEx, set_line)
 
     def test_display_size(self):
-        """
-        Simple test of size get method
+        r"""
+        Simple test of display_size get method
 
         """
-        self.assertIsInstance(self.lcd.display_size, tuple, 'Did not return tuple')
+        self.assertIsInstance(self.lcd.display_size, tuple, 'display_size() Did not return tuple')
 
-    @skip
     def test_display_cursor_set(self):
-        self.fail()
+        r"""
+        Simple test of display_size set method
 
-    @skip
+        """
+        self.lcd.display_cursor = True
+        self.assertIsInstance(self.lcd.display_cursor, bool, 'display_cursor() Did not return bool')
+        self.assertEqual(self.lcd.display_cursor, True, 'Unable to set cursor')
+        self.lcd.display_cursor = False
+        self.assertEqual(self.lcd.display_cursor, False, 'Unable to change cursor')
+
     def test_display_cursor(self):
-        self.fail()
+        r"""
+        Simple test of display_cursor get method
 
-    @skip
+        """
+        self.assertIsInstance(self.lcd.display_cursor, bool, 'display_cursor() Did not return Bool')
+
     def test_trigger_cursor_set(self):
-        self.fail()
+        self.lcd.columns = self.columns
+        self.lcd.lines = self.lines
+        self.display.set_backlight(1)
+        self.display.show_cursor(True)
+        self.display.blink(True)
+        self.lcd.trigger_cursor()
+        time.sleep(.1)
+        self.lcd.trigger_cursor((4, 1))
+        time.sleep(.1)
+        with pytest.raises(LcdScrollEx):
+            self.lcd.trigger_cursor((22, 5))
+        with pytest.raises(LcdScrollEx):
+            self.lcd.trigger_cursor((-12, 32))
+        self.lcd.trigger_cursor((0, 0))
+        cols = self.lcd.display_size[0]
+        for x in range(0, cols):
+            self.display.message(chr(ord('A') + x))
+        for x in range(0, cols):
+            self.lcd.trigger_cursor()
+            time.sleep(.5)
 
-    @skip
-    def test_trigger_letter(self):
-        self.fail()
+        self.display.set_backlight(0)
+        self.display.blink(False)
+        self.display.show_cursor(False)
 
-    @skip
     def test_send_character(self):
-        self.fail()
+        r"""
+        Stupid Test
+
+        """
+        self.lcd.send_character('a')
+        self.lcd.send_character('b')
+        with pytest.raises(LcdScrollEx):
+            self.lcd.send_character('asdasdasd')
+
 
     @skip
     def test_send_word(self):
